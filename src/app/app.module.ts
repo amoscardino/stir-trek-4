10ba8dc4f986a01a1b504a0bf596535cc927a369
Andrew Moscardino <andrew@moscardino.net>
2019-03-22 13:24:20 -0400

    Removed unfinished filter from session list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
@@ -10,24 +9,21 @@ import { SessionListComponent } from './session-list/session-list.component';
 import { SessionDetailComponent } from './session-detail/session-detail.component';
 import { SavedSessionListComponent } from './saved-session-list/saved-session-list.component';
 import { environment } from '../environments/environment';
-import { TimeSlotFilterPipe } from './time-slot-filter.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SessionListComponent,
-    SessionDetailComponent,
-    SavedSessionListComponent,
-    TimeSlotFilterPipe
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SessionListComponent,
+        SessionDetailComponent,
+        SavedSessionListComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/time-slot-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimeSlotFilterPipe } from './time-slot-filter.pipe';
-
-describe('TimeSlotFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimeSlotFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
